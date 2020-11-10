f3ceb4403e20bebfc062f5303dbc25afb53c2a30
Zac Oxendine <zachary.oxendine@icloud.com>
2020-11-09 19:57:25 -0600

    Finishing ProjectsView

---
+++ SwiftYooAi/SelectSomethingView.swift
@@ -0,0 +1,20 @@
+//
+//  SelectSomethingView.swift
+//  SwiftYooAi
+//
+//  Created by Zachary Oxendine on 11/9/20.
+//
+
+import SwiftUI
+
+struct SelectSomethingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SelectSomethingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectSomethingView()
+    }
+}
