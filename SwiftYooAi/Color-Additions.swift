e3c04a419c664fd5b848173610412fba17889f54
Zac Oxendine <zachary.oxendine@icloud.com>
2020-11-30 15:14:15 -0600

    creating the home view

---
+++ SwiftYooAi/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  SwiftYooAi
+//
+//  Created by Zachary Oxendine on 11/25/20.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
