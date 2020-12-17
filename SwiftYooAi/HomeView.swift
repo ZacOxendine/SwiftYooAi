044065a400029492cd6b6d98f42b98d4baf7477e
Zac Oxendine <zachary.oxendine@icloud.com>
2020-12-17 17:53:15 -0600

    core data delete rules and predicates

--- SwiftYooAi/HomeView.swift
+++ SwiftYooAi/HomeView.swift
@@ -24,24 +24,24 @@ struct HomeView: View {
     
     init() {
         let request: NSFetchRequest<Item> = Item.fetchRequest()
-        request.predicate = NSPredicate(format: "completed = false")
+        let completedPredicate = NSPredicate(format: "completed = false")
+        let openPredicate = NSPredicate(format: "project.closed = false")
+        let compoundPredicate = NSCompoundPredicate(type: .and, subpredicates: [completedPredicate, openPredicate])
+        
+        request.predicate = compoundPredicate
         
         request.sortDescriptors = [
             NSSortDescriptor(keyPath: \Item.priority, ascending: false)
         ]
         
         request.fetchLimit = 10
+        
         items = FetchRequest(fetchRequest: request)
     }
     
     var body: some View {
         NavigationView {
             ScrollView {
-                Button("Add Data") {
-                    dataController.deleteAll()
-                    try? dataController.createSampleData()
-                }
-                
                 VStack(alignment: .leading) {
                     ScrollView(.horizontal, showsIndicators: false) {
                         LazyHGrid(rows: projectRows) {
@@ -60,6 +60,13 @@ struct HomeView: View {
             }
             .background(Color.systemGroupedBackground.ignoresSafeArea())
             .navigationTitle("Home")
+            // temp toolbar
+            .toolbar {
+                Button("Add Data") {
+                    dataController.deleteAll()
+                    try? dataController.createSampleData()
+                }
+            }
         }
     }
 }
