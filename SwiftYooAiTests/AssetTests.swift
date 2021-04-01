9b0d3937215eacfe8ce233409038b6a1d8f534c3
Zac Oxendine <zachary.oxendine@icloud.com>
2021-04-01 12:36:14 -0500

    Testing Core Data

--- SwiftYooAiTests/AssetTests.swift
+++ SwiftYooAiTests/AssetTests.swift
@@ -1,5 +1,5 @@
 //
-//  AssetTest.swift
+//  AssetTests.swift
 //  SwiftYooAiTests
 //
 //  Created by Zachary Oxendine on 1/19/21.
@@ -8,7 +8,7 @@
 import XCTest
 @testable import SwiftYooAi
 
-class AssetTest: XCTestCase {
+class AssetTests: XCTestCase {
     func testColorsExist() {
         for color in Project.colors {
             XCTAssertNotNil(UIColor(named: color), "Failed to load color '\(color)' from asset catalog.")
